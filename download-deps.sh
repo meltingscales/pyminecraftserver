fa844dbb8f918f7469b604e39fd0fe9f486aef14
Henry Post <HenryFBP@gmail.com>
2019-09-16 20:24:51 -0500

    mkdir if not exist

--- download-deps.sh
+++ download-deps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DOWNLOAD_PATH="/minecraft/persistent/downloads/"
+mkdir -p "$DOWNLOAD_PATH"
 
 RLCRAFT_SERVER_PACK_URL="https://www.curseforge.com/minecraft/modpacks/rlcraft/download/2780296/file"
 RLCRAFT_SERVER_PACK_FILENAME="RLCraftServerPack-1.12.2-Beta-v2.5.zip"
@@ -8,5 +9,6 @@ RLCRAFT_SERVER_PACK_PATH="${DOWNLOAD_PATH}/${RLCRAFT_SERVER_PACK_FILENAME}"
 
 # Download server path if it does not exist
 if ! [ -f "$RLCRAFT_SERVER_PACK_PATH" ]; then
+    echo "Downloading server pack..."
     wget "$RLCRAFT_SERVER_PACK_URL" -O "$RLCRAFT_SERVER_PACK_PATH"
 fi

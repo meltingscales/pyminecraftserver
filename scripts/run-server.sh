22150bdf8d3dcd55bb5edf97edeb9b64bfa4629b
Henry Post <HenryFBP@gmail.com>
2019-09-17 16:02:49 -0500

    use portion of memory

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# KB of mem on this system.
+MEM_KB=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+
+# MB of mem on this system.
+MEM_MB=$(echo "$MEM_KB/1024" | bc)
+
+# Use 80% of memory.
+MEM_RATIO=0.8
+
+# Memory to use. Split off the dot.
+MEM_TO_USE=$(echo "$MEM_MB*$MEM_RATIO" | bc | cut -f1 -d.)
+
 if sudo netstat -tulpn | grep :25565; then
   echo 'Minecraft server is up because port 25565 is bound. See above command output for PID.'
 elif ps -ax | grep "[f]orge-.*-universal.jar"; then
@@ -7,11 +19,13 @@ elif ps -ax | grep "[f]orge-.*-universal.jar"; then
 else
   echo 'Minecraft server is not running.'
 
+  echo "Using ${MEM_TO_USE}MB of memory out of ${MEM_MB}MB with a ratio of ${MEM_RATIO} for Java."
+
   echo "Starting server..."
 
-  pushd /minecraft/server/
+  pushd /minecraft/persistent/server/
 
-  java -Xmx3G -Xms1G -jar "$(ls forge-*-universal.jar)"
+  java -Xmx${MEM_TO_USE}M -Xms1G -jar "$(ls forge-*-universal.jar)"
 
   popd
 fi

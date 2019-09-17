8f69ada9bf4728b1c330ea435c98ebfdd3984af6
Henry Post <HenryFBP@gmail.com>
2019-09-16 23:24:33 -0500

    install unzip

--- install-tools.sh
+++ install-tools.sh
@@ -5,3 +5,8 @@ if ! hash java &>/dev/null; then
   sudo apt-get update
   sudo apt-get install -y openjdk-8-jdk
 fi
+
+if ! hash unzip &>/dev/null; then
+  sudo apt-get update
+  sudo apt-get install unzip
+fi

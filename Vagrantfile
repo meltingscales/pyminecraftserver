44424371ab6d6db4cfd529c01ebce0e767bfe0ad
HenryFBP <HenryFBP@gmail.com>
2019-09-16 19:32:48 -0500

    download le file

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,11 @@ CORES = 4
 Vagrant.configure("2") do |config|
   config.vm.box = "hashicorp/precise64"
 
+  # Disable default synced folder
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+
+  config.vm.synced_folder "./persistent", "/minecraft/persistent/"
+
   config.vm.provision 'shell', run: 'once', path: 'download-deps.sh'
 
   config.vm.provider "virtualbox" do |v|
--- download-deps.sh
+++ download-deps.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-RLCRAFT_SERVER_URL="https://www.curseforge.com/minecraft/modpacks/rlcraft/download/2780296/file"
+DOWNLOAD_PATH="/minecraft/persistent/downloads/"
+
+RLCRAFT_SERVER_PACK_URL="https://www.curseforge.com/minecraft/modpacks/rlcraft/download/2780296/file"
+RLCRAFT_SERVER_PACK_FILENAME="RLCraftServerPack-1.12.2-Beta-v2.5.zip"
+RLCRAFT_SERVER_PACK_PATH="${DOWNLOAD_PATH}/${RLCRAFT_SERVER_PACK_FILENAME}"
+
+# Download server path if it does not exist
+if ! [ -f "$RLCRAFT_SERVER_PACK_PATH" ]; then
+    wget "$RLCRAFT_SERVER_PACK_URL" -O "$RLCRAFT_SERVER_PACK_PATH"
+fi

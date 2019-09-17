99000f962c3a124069e12bcb14f97bd033e53893
Henry Post <HenryFBP@gmail.com>
2019-09-16 20:36:42 -0500

    check integrity of server pack

--- download-deps.sh
+++ download-deps.sh
@@ -4,11 +4,19 @@ DOWNLOAD_PATH="/minecraft/persistent/downloads/"
 mkdir -p "$DOWNLOAD_PATH"
 
 RLCRAFT_SERVER_PACK_URL="https://www.curseforge.com/minecraft/modpacks/rlcraft/download/2780296/file"
-RLCRAFT_SERVER_PACK_FILENAME="RLCraftServerPack-1.12.2-Beta-v2.5.zip"
-RLCRAFT_SERVER_PACK_PATH="${DOWNLOAD_PATH}/${RLCRAFT_SERVER_PACK_FILENAME}"
+RLCRAFT_SERVER_PACK_PATH="${DOWNLOAD_PATH}/RLCraftServerPack-1.12.2-Beta-v2.5.zip"
+RLCRAFT_SERVER_CHECKSUM_FILE="${DOWNLOAD_PATH}/RLCraftServerPack-1.12.2-Beta-v2.5.chk"
 
 # Download server path if it does not exist
 if ! [ -f "$RLCRAFT_SERVER_PACK_PATH" ]; then
-    echo "Downloading server pack..."
-    wget "$RLCRAFT_SERVER_PACK_URL" -O "$RLCRAFT_SERVER_PACK_PATH"
+  echo "Downloading server pack..."
+  wget "$RLCRAFT_SERVER_PACK_URL" -O "$RLCRAFT_SERVER_PACK_PATH"
 fi
+
+pushd $DOWNLOAD_PATH
+if sha256sum --check "$RLCRAFT_SERVER_CHECKSUM_FILE"; then
+  echo 'Server pack file is valid.'
+else
+  echo "Server pack file at '#RLCRAFT_SERVER_PACK_PATH' is invalid. Re-download it!"
+fi
+popd

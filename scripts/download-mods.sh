8d8746d5c1bd89ecbec66bd9e954ca0af54caaf2
Henry Post <HenryFBP@gmail.com>
2019-09-17 19:58:07 -0500

    remove debug prints

--- scripts/download-mods.sh
+++ scripts/download-mods.sh
@@ -3,17 +3,8 @@
 # Import file lib fns
 . file-lib.sh 2>/dev/null || . $SCRIPT_DIR/file-lib.sh
 
-echo "hi lol"
-
-echo "I download mods...im not DONE YET >:("
-
-echo $DOWNLOAD_DIR
-echo $SERVER_DIR
-echo $SERVER_DIR/mods/
-
+# Mod directory
 MOD_DIR="${SERVER_DIR}/mods/"
-
-echo "oh look at you using env vars wow so portable :>)"
 
 while read line; do
 

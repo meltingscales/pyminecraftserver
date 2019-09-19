2cd8d7ec7c930a12a29fb6373fa8c49ae0f0f72c
Henry Post <HenryFBP@gmail.com>
2019-09-19 16:57:03 -0500

    simplify package installing code

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -2,31 +2,29 @@
 
 sudo apt-get update
 
+# Make sure that a command exists.
+# If not, install a package that provides it using apt.
+# ARG1 is the command name that can determine if a package is installed.
+# ARG2 is the package that needs to be installed.
+#
+function apt_ensure_exists() {
+    local COMMAND_NAME="$1"
+    local APT_PACKAGE_NAME="$2"
+
+    if ! hash "$COMMAND_NAME" &> /dev/null; then
+        sudo apt-get install -y "$APT_PACKAGE_NAME"
+    else
+        echo "${COMMAND_NAME} exists, not installing ${APT_PACKAGE_NAME}."
+    fi
+}
+
 # If java command does not exist, then install Java 8.
-if ! hash java &> /dev/null; then
-    sudo apt-get install -y openjdk-8-jdk
-fi
+apt_ensure_exists "java" "openjdk-8-jdk"
 
-if ! hash unzip &> /dev/null; then
-    sudo apt-get install -y unzip
-fi
-
-if ! hash tree &> /dev/null; then
-    sudo apt-get install -y tree
-fi
-
-if ! hash bc &> /dev/null; then
-    sudo apt-get install -y bc
-fi
-
-if ! hash htop &> /dev/null; then
-    sudo apt-get install -y htop
-fi
-
-if ! hash lynx &> /dev/null; then
-    sudo apt-get install -y lynx
-fi
-
-if ! hash dos2unix &> /dev/null; then
-    sudo apt-get install -y dos2unix
-fi
+apt_ensure_exists "unzip" "unzip"
+apt_ensure_exists "tree" "tree"
+apt_ensure_exists "bc" "bc"
+apt_ensure_exists "htop" "htop"
+apt_ensure_exists "lynx" "lynx"
+apt_ensure_exists "dos2unix" "dos2unix"
+apt_ensure_exists "jq" "jq"

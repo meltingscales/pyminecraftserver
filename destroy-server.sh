220e834f55279fe13038a04fe191eff83ba26c79
Henry Post <HenryFBP@gmail.com>
2019-09-19 17:03:11 -0500

    remove downloads too

--- destroy-server.sh
+++ destroy-server.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-read -p "Are you sure you want to destroy the server, and all saves and backups?
+read -p "Are you sure you want to destroy the downloads, server, and all saves and backups?
 (y/n) > " -n 1 -r
 echo # (optional) move to a new line
 if [[ "$REPLY" =~ ^[Yy] ]]; then
 
     rm ./persistent/server/ -rf
+    rm ./persistent/downloads/ -rf
 fi

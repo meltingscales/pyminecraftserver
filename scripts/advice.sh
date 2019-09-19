f6475ab6e878cf16cedb352c2c363e3926343372
Henry Post <HenryFBP@gmail.com>
2019-09-19 17:42:37 -0500

    mention cool feature

--- scripts/advice.sh
+++ scripts/advice.sh
@@ -2,5 +2,7 @@
 
 echo 'Run `vagrant ssh`, log in, run `cd '"${SCRIPT_DIR}"'`, and then run `./run-server.sh` to start the server.'
 
+echo 'Alternatively, you can run "run-server.sh" on your host machine.'
+
 echo 'It will also check if the server is already running.'
 

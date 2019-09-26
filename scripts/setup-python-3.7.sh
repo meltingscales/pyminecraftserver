846222480b8809636d2e690aa0c19ddd529c53f0
Henry Post <HenryFBP@gmail.com>
2019-09-26 00:46:57 -0500

    fuck

--- scripts/setup-python-3.7.sh
+++ scripts/setup-python-3.7.sh
@@ -9,6 +9,7 @@ if ! hash python3.7 ; then
     pushd /tmp/py3.7
 
     # Prepare to install py3.7
+    sudo apt update
     sudo apt-get install -y libffi-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev openssl
 
     # DL source

49dec3a8023cef97df958e8688ec46cbefeb4419
Henry Post <HenryFBP@gmail.com>
2019-10-20 18:04:20 -0500

    use uuid for tmp dir if running concurrently

--- test/sharedTest.py
+++ test/sharedTest.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 import unittest
+import uuid
 
 
 class TestCaseWithEphemeralTempDir(unittest.TestCase):
-    temp_dir = os.path.join(os.path.dirname(__file__), 'tmp')
+    temp_dir = os.path.join(os.path.dirname(__file__), 'tmp', str(uuid.uuid4()))
 
     def setUp(self) -> None:
         if os.path.exists(self.temp_dir):

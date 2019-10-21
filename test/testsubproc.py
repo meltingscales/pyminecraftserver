89feab027518e3ed3cd4604ac90d44dc918babe5
Henry Post <HenryFBP@gmail.com>
2019-10-20 19:54:02 -0500

    ok linux you too can open files

--- test/testsubproc.py
+++ test/testsubproc.py
@@ -2,8 +2,11 @@ import os
 import subprocess
 import time
 
-if __name__ == '__main__' and os.name == 'nt':
-    myproc = subprocess.Popen(['notepad.exe', os.path.abspath(__file__)])
+if __name__ == '__main__':
+    if os.name == 'nt':
+        myproc = subprocess.Popen(['notepad.exe', os.path.abspath(__file__)])
+    else:
+        myproc=subprocess.Popen(['gedit', os.path.abspath(__file__)])
 
     time.sleep(2)
 

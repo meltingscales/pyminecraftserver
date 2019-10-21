6313f08b1c21226beef4287bf934cf403327d951
Henry Post <HenryFBP@gmail.com>
2019-10-20 19:46:16 -0500

    just use tempfile lib

--- test/sharedTest.py
+++ test/sharedTest.py
@@ -1,18 +1,10 @@
 import os
 import shutil
+import tempfile
+import time
 import unittest
 import uuid
 
 
 class TestCaseWithEphemeralTempDir(unittest.TestCase):
-    temp_dir = os.path.join(os.path.dirname(__file__), 'tmp', str(uuid.uuid4()))
-
-    def setUp(self) -> None:
-        if os.path.exists(self.temp_dir):
-            shutil.rmtree(self.temp_dir)
-        else:
-            os.mkdir(self.temp_dir)
-
-    def tearDown(self) -> None:
-        if os.path.exists(self.temp_dir):
-            shutil.rmtree(self.temp_dir)
+    temp_dir = tempfile.mkdtemp()
